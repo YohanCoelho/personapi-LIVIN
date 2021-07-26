62d70df91163786225956a34cd2b40b8a363626f
YohanCoelho <yohancoelho08@outlook.com>
2021-07-26 13:22:03 -0300

    Developed create person operation2

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,16 +1,32 @@
 package one.digitalinnovation.personapi.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook() {
-        return "Bem vindo!";
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person ) {
+        return personService.createPerson(person);
     }
 }
